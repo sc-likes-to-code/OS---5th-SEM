5f3172414ca3a8026f3939ad51970e634bc0f410
Shourjya Chakraborty <137634522+sc-likes-to-code@users.noreply.github.com>
2025-12-21 04:09:16 +0530

    Refactor factorial calculation using pipes
    Updated the factorial calculation to use a pipe for inter-process communication, allowing the program to send and receive the number for which the factorial is calculated.

--- sec A_sem prac_solns/fact_pipe.c
+++ sec A_sem prac_solns/fact_pipe.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
+#include <string.h>
 
 int main()
 {
-    int fd[2], n;
+    int p[2], r, num;
     long long fact = 1;
-    pipe(fd);
+    char buf[100];
 
-    printf("Enter number: ");
-    scanf("%d", &n);
+    printf("\tFactorial of a Number using PIPE\n");
 
-    if(fork() == 0)
+    r = pipe(p);
+    if (r < 0)
     {
-        close(fd[0]);
-        for(int i = 1; i <= n; i++)
-            fact *= i;
-        write(fd[1], &fact, sizeof(fact));
-        close(fd[1]);
+        printf("Failed to create unnamed pipe...\n");
+        exit(1);
     }
-    else
-    {
-        close(fd[1]);
-        read(fd[0], &fact, sizeof(fact));
-        printf("Factorial = %lld\n", fact);
-        close(fd[0]);
-        wait(NULL);
-    }
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Process 1: send number
+    write(p[1], &num, sizeof(num));
+
+    // Process 2: read number and calculate factorial
+    read(p[0], &num, sizeof(num));
+
+    for (int i = 1; i <= num; i++)
+        fact = fact * i;
+
+    sprintf(buf, "Factorial of %d is %lld\n", num, fact);
+    printf("%s", buf);
+
     return 0;
 }
